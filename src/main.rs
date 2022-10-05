15e3c453d4e0755a9c7fce20f2934a659288815e
nero-dv <louis_noir@protonmail.com>
2022-10-05 06:00:39 -0500

    Changed datatype to u128

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::io;
 
 fn main() {
-    let mut n1 = 0;
-    let mut n2 = 1;
-    let mut count = 0;
+    let mut n1: u128 = 0;
+    let mut n2: u128 = 1;
+    let mut count: u128 = 0;
 
     loop {
-        println!("Enter terms for Fibonacci sequence (under 46 as this program is designed to work with unsigned integers up to 32 bits in size)");
+        println!("Enter terms for Fibonacci sequence.\nConstraints: Greater than 0, less than 186");
         let mut nterm: String = String::new();
 
         io::stdin()
             .read_line(&mut nterm)
             .expect("Failed to read line.");
 
-        let nterm: u32 = match nterm.trim().parse() {
+        let nterm: u128 = match nterm.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -24,9 +24,9 @@ fn main() {
         } else {
             println!("Fibonacci sequence:");
             while count < nterm {
-                let run: u32 = count + 1;
+                let run: u128 = count + 1;
                 println!("Run: {run}, nth sequence: {n1}");
-                let nth: u32 = n1 + n2;
+                let nth: u128 = n1 + n2;
                 n1 = n2;
                 n2 = nth;
                 count += 1;
